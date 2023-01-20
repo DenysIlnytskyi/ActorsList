a371c495edf04aa54373da7aafde4fb855836125
DenysIlnytskyi <380984511271k@gmail.com>
2023-01-20 17:02:35 +0100

    Initial Commit

---
+++ 44444/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  44444
+//
+//  Created by Денис Ільницький on 20/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
